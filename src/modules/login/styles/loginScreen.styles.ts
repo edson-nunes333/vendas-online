c2ac911e97fe68d87b7a01e4c2192d2bff453a36
edson-nunes333 <edsonpatricio333@gmail.com>
2023-11-19 00:09:45 -0300

    feat: create module login

---
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BackgroundImage = styled.img`
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    object-fit: cover;
+`;
